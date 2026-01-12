6f6ff892a963b9314bcec3103ec170c2b7b250f1
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-01-12 17:26:30 +0000

    Sharing Logic with 'unknown' & Type Casting

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,23 @@
-import type { ComponentPropsWithRef } from 'react'
+import type { ComponentPropsWithRef, FormEvent } from 'react'
 
-type FormProps = ComponentPropsWithRef<'form'>
+type FormProps = ComponentPropsWithRef<'form'> & {
+  onSave: (value: unknown) => void
+}
 
-function Form(props: FormProps) {
-  return <form {...props}>{props.children}</form>
+function Form({ onSave, children, ...otherProps }: FormProps) {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const data = Object.fromEntries(formData)
+    onSave(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} {...otherProps}>
+      {children}
+    </form>
+  )
 }
 
 export default Form
