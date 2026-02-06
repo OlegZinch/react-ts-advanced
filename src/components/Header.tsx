ef60dd890b071154ce920cbfec24639c3abb8a8b
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-02-06 13:56:19 +0000

    Accessing Context Type-Safe With A Custom Hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
+import { useTimersContext } from '../store/timers-context.tsx'
+
 import Button from './UI/Button.tsx'
 
 export default function Header() {
+  const { isRunning } = useTimersContext()
+
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button>{isRunning ? 'Stop' : 'Start'}&nbsp;Timers</Button>
     </header>
   )
 }
